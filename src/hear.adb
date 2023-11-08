ec46a508136e54eecac8c59b0c006800b43d7e58
Ava Crocker <avacrocker03@gmail.com>
2023-11-08 13:43:55 -0500

    the start of something good

--- src/hear.adb
+++ src/hear.adb
@@ -5,45 +5,23 @@
 --  Language implementation: GNAT Community 2021 (20210519-103)
 
 with Ada.Text_IO;
-with Ada.Containers;
-with Ada.Containers.Vectors;
-use Ada.Containers;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO.Unbounded_IO;
+use Ada.Strings.Unbounded;
 
 procedure Hear is
 
-   Line   : String (1 .. 150);
-   Length : Natural;
-
-   type Constrained_String is new String (1 .. 150);
-
-   package Vector_Package is new Ada.Containers.Vectors -- defines package
-    (Index_Type => Positive, -- defines index
-    Element_Type => Constrained_String); -- defines type
-   Input_Vector : Vector_Package.Vector;
-
-   function Convert_Constrained (Str : String) return Constrained_String;
-
-   function Convert_Constrained (Str : String) return Constrained_String is
-      Result : Constrained_String := (others => ' ');
-   begin
-      for I in Str'Range loop
-         Result (I) := Str (I);
-      end loop;
-      return Result;
-   end Convert_Constrained;
+   Line : Unbounded_String;
+   First_Tower : String (1 .. 50);
+   Second_Tower : String (1 .. 50);
 
 begin
 
-   Vector_Package.Clear (Input_Vector);
-
    while not (Ada.Text_IO.End_Of_File) loop
-      Ada.Text_IO.Get_Line (Ada.Text_IO.Standard_Input, Line, Length);
-      Vector_Package.Append (Input_Vector, Convert_Constrained (Line));
+      Ada.Text_IO.Unbounded_IO.Get_Line (Ada.Text_IO.Standard_Input, Line);
+      First_Tower := Ada.Strings.Unbounded.Slice (Line, 1, 50);
+      Ada.Text_IO.Put_Line (Item => Slice_Str);
+      Ada.Text_IO.Put_Line ("-");
    end loop;
 
-
---     for I in Vector_Package.Iterate (Input_Vector) loop
---        Ada.Text_IO.Put (Vector_Package.Element (Input_Vector, I));
---        Ada.Text_IO.New_Line;
---     end loop;
 end Hear;
