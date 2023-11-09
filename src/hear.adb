a1e917e16bca248947c01245845fe973bba7f6f6
Ava Crocker <avacrocker03@gmail.com>
2023-11-09 00:19:06 -0500

    towers stored

--- src/hear.adb
+++ src/hear.adb
@@ -12,16 +12,62 @@ use Ada.Strings.Unbounded;
 procedure Hear is
 
    Line : Unbounded_String;
-   First_Tower : String (1 .. 50);
-   Second_Tower : String (1 .. 50);
+   First_Tower : Unbounded_String;
+   Second_Tower : Unbounded_String;
+   Pos1 : Natural;
+   Pos2 : Natural;
+   Length_UB : Natural;
 
 begin
 
    while not (Ada.Text_IO.End_Of_File) loop
       Ada.Text_IO.Unbounded_IO.Get_Line (Ada.Text_IO.Standard_Input, Line);
-      First_Tower := Ada.Strings.Unbounded.Slice (Line, 1, 50);
-      Ada.Text_IO.Put_Line (Item => Slice_Str);
-      Ada.Text_IO.Put_Line ("-");
+      Length_UB := Length (Line);
+
+      Pos1 := 1;
+      Pos2 := 1;
+
+      while Length_UB >= Pos1 and then Element (Line, Pos1) = ' ' loop
+         Pos1 := Pos1 + 1;
+      end loop;
+
+      Pos2 := Pos1;
+
+      while Length_UB >= Pos2 and then Element (Line, Pos2) /= ' ' loop
+         Pos2 := Pos2 + 1;
+      end loop;
+
+      if Length_UB < Pos1 then
+         exit;
+      end if;
+
+    --    Ada.Text_IO.Put (Integer'Image (Pos1));
+    --    Ada.Text_IO.Put (Integer'Image (Pos2));
+      First_Tower := To_Unbounded_String (Ada.Strings.Unbounded.Slice -- blah
+       (Line, Pos1, Pos2 - 1));
+      Ada.Text_IO.Unbounded_IO.Put_Line (Item => First_Tower);
+      Pos1 := Pos2 + 1;
+
+      while Length_UB >= Pos1 and then Element (Line, Pos1) = ' ' loop
+         Pos1 := Pos1 + 1;
+      end loop;
+
+      Pos2 := Pos1;
+
+      while Length_UB >= Pos2 and then Element (Line, Pos2) /= ' ' loop
+         Pos2 := Pos2 + 1;
+      end loop;
+
+      if Length_UB < Pos1 then
+         exit;
+      end if;
+
+      Second_Tower := To_Unbounded_String (Ada.Strings.Unbounded.Slice -- blah
+       (Line, Pos1, Pos2 - 1));
+      Ada.Text_IO.Unbounded_IO.Put_Line (Item => Second_Tower);
+
+      Ada.Text_IO.Put_Line ("--");
+
    end loop;
 
 end Hear;
